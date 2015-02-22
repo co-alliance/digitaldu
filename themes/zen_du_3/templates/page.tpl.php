04f0bba666af6bef3dc3ba852e14c3c29ed18542
Jeff <you@example.com>
2015-02-22 11:04:12 -0700

    Path test

--- themes/zen_du_3/templates/page.tpl.php
+++ themes/zen_du_3/templates/page.tpl.php
@@ -10,10 +10,8 @@
 
 <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
 
-<!-- <script src="../sites/all/themes/zen_du_3/js/jquery.min.js"></script>
-<script src="../sites/all/themes/zen_du_3/js/jssor.slider.mini.js"></script> -->
-<script src="../js/jquery.min.js"></script>
-<script src="../js/jssor.slider.mini.js"></script>
+<script src="../sites/all/themes/zen_du_3/js/jquery.min.js"></script>
+<script src="../sites/all/themes/zen_du_3/js/jssor.slider.mini.js"></script>
 <script>
     jQuery(document).ready(function ($) {
         var options = { $AutoPlay: true };
@@ -30,7 +28,7 @@
     </div>
 
     <div id="du-title-header-inner">
-      <span id="site-title">Digital DU</span>
+      <span id="site-title">Digital DU<?php echo getcwd(); ?></span>
       <span id="upper-header"><?php print render($page['upper_header']); ?></span>
     </div>
   </header>
